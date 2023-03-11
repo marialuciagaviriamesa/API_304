868afd6ba18d38d51bb599c0f45a2753ca1edc50
Maria Lucia <marialuciagaviriamesa@gmail.com>
2023-03-11 08:15:52 -0500

    Api file #3

--- auth_service/src/index.ts
+++ auth_service/src/index.ts
@@ -1,1 +1,8 @@
-console.log("HOLA")
+/*console.log("HOLA")*/
+import express, {Request,Response} from 'express';
+const server = express();
+
+server.get('/api', (req: Request, res:Response)=> {
+    res.send({message:'Hello world!!'})
+})
+server.listen(80,()=>{console.log('App is up and running')});
